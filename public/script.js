500bf979e9743cc55a47ab4901d9f1ac931366ea
Razdan123 <razdanrohit73@gmail.com>
2022-01-30 06:59:39 +0530

    color alignment

--- public/script.js
+++ public/script.js
@@ -34,7 +34,7 @@ const editor = grapesjs.init({
         select: true,
         // You can pass components as a JSON instead of a simple HTML string,
         // in this case we also use a defined component type `image`
-        content:{ type: 'image' },
+        content:{ type: 'image' }, 
         // This triggers `active` event on dropped components and the `image`
         // reacts by opening the AssetManager
         activate: true,
@@ -55,7 +55,16 @@ const editor = grapesjs.init({
       }
     ]
   },
-
+  deviceManager: {
+    devices: [{
+        name: 'Desktop',
+        width: '', // default size
+      }, {
+        name: 'Mobile',
+        width: '320px', // this value will be used on canvas width
+        widthMedia: '480px', // this value will be used in CSS @media
+    }]
+  },
   layerManager: {
     appendTo: '.layers-container'
   },
@@ -76,9 +85,103 @@ const editor = grapesjs.init({
         // instead of the `width` (default)
         keyWidth: 'flex-basis',
       },
+      
+    },
+    {
+      id: 'panel-switcher',
+      el: '.panel__switcher',
+      buttons: [{
+          id: 'show-layers',
+          active: true,
+          label: 'Layers',
+          command: 'show-layers',
+          // Once activated disable the possibility to turn it off
+          togglable: false,
+        }, {
+          id: 'show-style',
+          active: true,
+          label: 'Styles',
+          command: 'show-styles',
+          togglable: false,
+      }],
+    },
+    {
+      id: 'panel-switcher',
+      el: '.panel__switcher',
+      buttons: [
+        // ...
+        {
+          id: 'show-traits',
+          active: true,
+          label: 'Traits',
+          command: 'show-traits',
+          togglable: false,
+      }],
+    },
+    {
+      id: 'panel-devices',
+      el: '.panel__devices',
+      buttons: [{
+          id: 'device-desktop',
+          label: 'D',
+          command: 'set-device-desktop',
+          active: true,
+          togglable: false,
+        }, {
+          id: 'device-mobile',
+          label: 'M',
+          command: 'set-device-mobile',
+          togglable: false,
+      }],
     }]
-  }
-  
+  },
+  traitManager: {
+    appendTo: '.traits-container',
+  },
+  selectorManager: {
+    appendTo: '.styles-container'
+  },
+  styleManager: {
+    appendTo: '.styles-container',
+    sectors: [{
+        name: 'Dimension',
+        open: false,
+        // Use built-in properties
+        buildProps: ['width', 'min-height', 'padding','margin','display'],
+        // Use `properties` to define/override single property
+        properties: [
+          {
+            // Type of the input,
+            // options: integer | radio | select | color | slider | file | composite | stack
+            type: 'integer',
+            name: 'The width', // Label for the property
+            property: 'width', // CSS property (if buildProps contains it will be extended)
+            units: ['px', '%'], // Units, available only for 'integer' types
+            defaults: 'auto', // Default value
+            min: 0, // Min value, available only for 'integer' types
+          }
+        ]
+      },{
+        name: 'Extra',
+        open: false,
+        buildProps: ['background-color', 'box-shadow', 'custom-prop'],
+        properties: [
+          {
+            id: 'custom-prop',
+            name: 'Custom Label',
+            property: 'font-size',
+            type: 'select',
+            defaults: '32px',
+            // List of options, available only for 'select' and 'radio'  types
+            options: [
+              { value: '12px', name: 'Tiny' },
+              { value: '18px', name: 'Medium' },
+              { value: '32px', name: 'Big' },
+            ],
+         }
+        ]
+      }]
+  },
 });
 
 editor.BlockManager.add('my-block-id', {
@@ -135,4 +238,51 @@ editor.Panels.addPanel({
       },
     }
   ],
+});
+
+// Define commands
+editor.Commands.add('show-layers', {
+  getRowEl(editor) { return editor.getContainer().closest('.editor-row'); },
+  getLayersEl(row) { return row.querySelector('.layers-container') },
+
+  run(editor, sender) {
+    const lmEl = this.getLayersEl(this.getRowEl(editor));
+    lmEl.style.display = '';
+  },
+  stop(editor, sender) {
+    const lmEl = this.getLayersEl(this.getRowEl(editor));
+    lmEl.style.display = 'none';
+  },
+});
+editor.Commands.add('show-styles', {
+  getRowEl(editor) { return editor.getContainer().closest('.editor-row'); },
+  getStyleEl(row) { return row.querySelector('.styles-container') },
+
+  run(editor, sender) {
+    const smEl = this.getStyleEl(this.getRowEl(editor));
+    smEl.style.display = '';
+  },
+  stop(editor, sender) {
+    const smEl = this.getStyleEl(this.getRowEl(editor));
+    smEl.style.display = 'none';
+  },
+});
+
+editor.Commands.add('show-traits', {
+  getTraitsEl(editor) {
+    const row = editor.getContainer().closest('.editor-row');
+    return row.querySelector('.traits-container');
+  },
+  run(editor, sender) {
+    this.getTraitsEl(editor).style.display = '';
+  },
+  stop(editor, sender) {
+    this.getTraitsEl(editor).style.display = 'none';
+  },
+});
+editor.Commands.add('set-device-desktop', {
+  run: editor => editor.setDevice('Desktop')
+});
+editor.Commands.add('set-device-mobile', {
+  run: editor => editor.setDevice('Mobile')
 });
